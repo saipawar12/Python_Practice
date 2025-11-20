1c49d974d28d86759c233b769d89319696440bf7
saipawar12 <saipawar1218@gmail.com>
2025-11-20 13:01:09 +0530

    today

--- 100/string.py
+++ 100/string.py
@@ -1,26 +1,30 @@
-#string
+#accessing 
+#indexing
 # s="Hello World"
 # print(s[0])
 # print(s[1])
 # print(s[2])
 # print(s[3])
 # print(s[4])
-# print(s[-11])
-# print(s[-10])
-# print(s[-9])
-# print(s[-8])
-# print(s[-7])
-# print(s[-6])
-# print(s[-5])
-# print(s[-4])
-# print(s[-3])
-# print(s[-2])
-# print(s[-1])
 # print("length of hello is:",len(s))
 # print(s)
 
 #slicing
 s="Hello World"
 print(s[0:3]) #Hel
+print(s[-11:-1])
+print("reverse string is:",s[::-1]) #reverse
+
+# using for loop
+print("***************for loop**************")
+for i in "I am simple":
+    print(i)
+#while loop
+print("***************while loop**************")
+i=0
+while(i<len(s)):
+    print(s[i])
+    i+=1
+
 
 
