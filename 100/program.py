b2d0367a54ca2740368d45d6c189d537d6d461ac
saipawar12 <saipawar1218@gmail.com>
2025-12-04 19:28:57 +0530

    day7

---
+++ 100/program.py
@@ -0,0 +1,111 @@
+#1)WAP check num even or odd
+# num=23
+# if num%2==0:
+#     print("Even")
+# else:
+#     print("odd")
+
+#2)WAP take user input check number is positive or negative
+# n=int(input("enter the number:"))
+# if n>0:
+#     print("number is positive")
+# elif n<0:
+#     print("number is negative")
+# else:
+#     print("nunber is zero")
+
+#3)count the number of vowels of given string
+# s="I am sai pawar"
+# count=0
+# for i in s:
+#     if i in "aeiouAEIOU":
+#         count +=1
+# print(count)
+
+#4)reverse a string without using inbuilt function
+# s="Python"
+# rev=""
+# for i in s:
+#     rev=i+rev
+# print(rev)
+
+#5)check if string is palindrome or not
+# s="momy"
+# rev=""
+# for i in s:
+#     rev=i+rev
+# if s==rev:
+#     print("Palindrome")
+# else:
+#     print("Not palindrome")
+
+#6)find the largest number of three number
+# a=45
+# b=1200
+# c=110
+# if a>b and a>c:
+#     print("a is largest")
+# elif b>c:
+#     print("b is largest")
+# else:
+#     print("c is largest")
+
+#7)convert celsius temperature to fehrenheit
+# c=float(input("Enter temperature in Celsius:"))
+# f=(c*9/5)+32
+# print(f)
+
+#8)find the factorial of a number using loop
+# n=5
+# fact=1
+# for i in range(1,n+1):
+#     fact *=i
+# print(fact)
+
+#9)generate fibonacci series up to n terms
+# a = 0
+# b = 1
+# while a<=100:
+#     print(a,end='-')
+#     c=a+b
+#     a=b
+#     b=c
+
+#10)count digit in integer
+# n=1234987654315678
+# count=0
+# tem=n
+# while tem >0:
+#     tem //=10
+#     count +=1
+# print(count)
+
+#using for loop
+# digit_count=0
+# for i in str(n):
+#     digit_count +=1
+# print(digit_count)
+
+#11)calculate the sum of digit  of number
+# n=1234
+# sum=0
+# for i in str(n):
+#     sum += int(i)
+# print(sum)
+
+#12)swap two number without using a third variable
+# a=15
+# b=20
+
+
+#13)print the multiplication table og given number
+n=5
+for i in range(1,11):
+    print(i*n)
+
+#14)
+
+
+
+
+
