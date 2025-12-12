2564001b5587dac6be6faca9b42503a8b2725533
saipawar12 <saipawar1218@gmail.com>
2025-12-12 23:27:37 +0530

    day8

--- 100/dict_program.py
+++ 100/dict_program.py
@@ -12,17 +12,44 @@
 # average=sum/count
 # print(average)
 
-student={"Sai":67,
-         "ranjit":98,
-         "komal":85,
-         "nikhil":72}
-for i,j in student.items():
-    if j>90:
-        print(i,"grade A")
-    elif j>80:
-        print(i,"grade B")
-    elif j>70:
-        print(i,"grade C")
-    else:
-        print(i,"-fail")
+# student={"Sai":67,
+#          "ranjit":98,
+#          "komal":85,
+#          "nikhil":72}
+# for i,j in student.items():
+#     if j>90:
+#         print(i,"grade A")
+#     elif j>80:
+#         print(i,"grade B")
+#     elif j>70:
+#         print(i,"grade C")
+#     else:
+#         print(i,"-fail")
+
+#create a dictionary and add one more specific name
+# names={1:"Sai",
+#        2:"Ranjit",
+#        3:"Komal",
+#        5:"nikhil"}
+# names[6]="Sairaj"
+# print(names)
+
+#check key is exixt or not
+# names={1:"Sai",
+#        2:"Ranjit",
+#        3:"Komal",
+#        5:"nikhil"}
+ 
+# if "Sai" in names.values():
+#     print("found")
+# else:
+#     print("Not fount")
+
+#Write a program to increase the price of each product by 10% if the original price is above 100.
+fruits={"apple":90, "grapes":120,"straberry":80}
+for i, j in fruits.items():
+    if j>100:
+        fruits[i]=j*1.10
+print(fruits)
+
     
--- 100/set.py
+++ 100/set.py
@@ -4,7 +4,37 @@
 # print(type(set1))
 
 set1={1,2,3.5,"hello",1,1,11,3,5,2,2,3,4,11}
-print(set1)
+# print(set1)
+
+#accessing only for loop
+# for i in set1:
+#     print(i)
+
+#build in methods
+#1)set
+# set1=set()
+# print(type(set1))
+#2)len
+# print(len(set1))
+#3)ADD
+# set1.add(78)
+# print(set1)
+#4)update()
+# set1.update([100,32,454])
+# print(set1)
+#5)remove()
+# set1.remove(3.5)
+# print(set1)
+#6)discard()
+# set1.discard(100)
+# print(set1)
+#7)clear
+# set1.clear()
+# print(set1)
+#8)pop
+# set1.pop()
+# print(set1)
+
 
     
 
