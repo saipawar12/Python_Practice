9d86f22870794ffae35af66fab393e75bf219f30
saipawar12 <saipawar1218@gmail.com>
2026-01-05 11:28:41 +0530

    day14

--- 100/conditional_Program.py
+++ 100/conditional_Program.py
@@ -224,8 +224,8 @@
 #     print("Multi digit")
 
 #Check if a number is a multiple of 7 or ends with 7. 
-num = int(input("Enter a number: "))
-if num % 7 == 0 or num % 10 == 7:
-    print("The number is a multiple of 7 or ends with 7")
-else:
-    print("The number is NOT a multiple of 7 and does NOT end with 7")
+# num = int(input("Enter a number: "))
+# if num % 7 == 0 or num % 10 == 7:
+#     print("The number is a multiple of 7 or ends with 7")
+# else:
+#     print("The number is NOT a multiple of 7 and does NOT end with 7")
