7f1d80b595aa18ff4b494a9059dab4dfcd8e8f6b
saipawar12 <saipawar1218@gmail.com>
2026-01-06 13:05:52 +0530

    day14

--- 100/Except.py
+++ 100/Except.py
@@ -7,10 +7,36 @@
 # print("hello")
     
 
+# try:
+#     f=open("file4.txt","r")
+#     print(f.read())
+#     f.close()
+# except:
+#     print("File not found in folder")
+# print("please create file")
+
+
+# try:
+#     a=int(input("Enter the value of a:"))
+#     b=int(input("Enter the value of b:"))
+#     print(a/b)
+# except ZeroDivisionError:
+#     print("We can't divisible by zero")
+# except ValueError:
+#     print("Enter the number only")
+# print("Hello")
+
+
 try:
-    f=open("file4.txt","r")
-    print(f.read())
-    f.close()
-except:
-    print("File not found in folder")
-print("please create file")
+    a=int(input("Enter the value of a:"))
+    b=int(input("Enter the value of b:"))
+    print(a/b)
+except ZeroDivisionError:
+    print("We can't divisible by zero")
+except ValueError:
+    print("Enter the number only")
+else:
+    print("No exception")
+finally:
+    print("hello")
+
